4c0d7bae8508fe83527a9f56fba8003c054c88a0
Alex Tetervak <tetervak@sheridancollege.ca>
2024-03-12 22:45:02 -0400

    added cross-origin

--- src/main/java/ca/tetervak/dicerollerservice/controller/GameServiceController.java
+++ src/main/java/ca/tetervak/dicerollerservice/controller/GameServiceController.java
@@ -4,10 +4,7 @@ import ca.tetervak.dicerollerservice.service.DiceRollData;
 import ca.tetervak.dicerollerservice.service.DiceRollerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -23,6 +20,7 @@ public class GameServiceController {
     }
 
     @GetMapping( "/roll-dice")
+    @CrossOrigin(origins = "http://localhost:4200/")
     public DiceRollData rollDice(
             @RequestParam(defaultValue = "3") int numberOfDice
     ){
